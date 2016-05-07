c964550cb7847d83b45224dceffcd0beed9a85c7
Kwan Ling <m.kwanling@gmail.com>
2016-05-07 01:54:40 -0400

    change in node express

---
+++ Readme.md
@@ -0,0 +1,13 @@
+
+
+# init seed project
+
+- npm install
+- npm run gulp
+- npm start
+
+# change in node
+
+- add node.hbs
+- add form
+- add post / params
--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('index');
+router.get('/:name?', function(req, res, next) {
+    res.render('node', {name: req.params.name ? req.params.name : ''});
 });
+
+
+router.post('/', function(req, res, next) {
+    var name = req.body.name;
+
+    res.redirect('/' + name);
+})
 
 module.exports = router;
